79db3ef368a49197c60eb07ced1f5b8db9ebb590
Kaio Rodrigo <120115578+kaiorodrigo@users.noreply.github.com>
2024-03-31 20:41:31 -0300

    Update generate-array.js
    update link

--- generatekayword/src/generate-array.js
+++ generatekayword/src/generate-array.js
@@ -1,7 +1,7 @@
 export default function generatedarray() {
   function define_kay_paragrafo() {
     let arrayLink =
-      "https://gist.githubusercontent.com/kayooooooo/04019210fb51e78638e5ebdfdb059177/raw/db329b826d2a7ecb7780094499f509a756dec675/array";
+      "https://gistcdn.githack.com/kayooooooo/04019210fb51e78638e5ebdfdb059177/raw/9ffc5dd17e0527d548e508a0bdfb954b3fe3ebe4/array";
 
     fetch(arrayLink)
       .then((response) => response.text())
